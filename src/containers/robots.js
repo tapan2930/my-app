3f1f220299f4b0cab3a718cf7f638ffbed634d13
Pooja-Ranpura <poojaranpura999@gmail.com>
2021-11-16 13:32:01 +0530

    add src

---
+++ src/containers/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Pooja Ranpura',
+    username: 'Pooja',
+    email: 'pooja123@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Ronak Ranpura',
+    username: 'Ronak',
+    email: 'ronak@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Kinjal Bhavsar',
+    username: 'Kinjal',
+    email: 'kinjal@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Surabhi Bhavsar',
+    username: 'Surabhi',
+    email: 'surabhi.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Nishita Bhavsar',
+    username: 'Nishita',
+    email: 'nishita@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Rushi Ranpura',
+    username: 'Rushi',
+    email: 'rushi_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Bansari Ranpura',
+    username: 'Bansari',
+    email: 'bansari.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
